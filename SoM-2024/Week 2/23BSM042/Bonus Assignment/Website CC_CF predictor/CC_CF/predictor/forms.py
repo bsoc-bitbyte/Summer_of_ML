68ea1c7e7bb8c33f6216302fef4f00e1a84c1398
Shivansh Fulper <piyushfulper3210@gmail.com>
2024-06-28 18:11:44 +0530

    Delete SoM-2024/Week 2/23BSM042 directory

--- SoM-2024/Week 2/23BSM042/Bonus Assignment/Website CC_CF predictor/CC_CF/predictor/forms.py
+++
@@ -1,9 +0,0 @@
-# predictor/forms.py
-
-from django import forms
-
-class CodeforcesToCodechefForm(forms.Form):
-    codeforces_rating = forms.IntegerField(label='Enter Codeforces Rating')
-
-class CodechefToCodeforcesForm(forms.Form):
-    codechef_rating = forms.IntegerField(label='Enter CodeChef Rating')
